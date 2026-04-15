fc033cedefb889bffe3449dd0bcf85b2f97dd366
y_coder <y_coder@qq.com>
2026-04-15 11:06:55 +0800

    Revert "feat: 隐藏文章标题下的作者"
    This reverts commit 87ae95e502d199c36ecd20d0bb8d810dad425420.

--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -20,7 +20,6 @@ export const useHopeTheme = ({ type, plat="" }) =>
     // 当前网站部署到的域名
     hostname: "https://atkdocs.smsat.space",
 
-    displayAuthor: false,
     author: {
       name: "国防科技大学空天科学学院",
       email: "atk_nudt@163.com",
