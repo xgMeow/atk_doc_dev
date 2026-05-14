3bc8e0c7731eac7e840c4f39a27fb88a455c1129
y_coder <y_coder@qq.com>
2026-05-14 17:12:09 +0800

    fix(config): 将 siteBase 路径修改为根路径

--- .vuepress/shared/config.js
+++ .vuepress/shared/config.js
@@ -2,4 +2,4 @@
 // 应该配置成GitHub Pages 部署路径，否则图片资源等会加载不正确。
 // 例如仓库名是 atk_doc，所以网站在 /atk_doc/ 子目录下，那么base就要配置成'/atk_doc/'
 // overview组件也要基于这个地址进行调整，否则在overview页面中点击链接会跳转到根路径下，导致资源加载失败。
-export const siteBase = '/atk_doc/'
+export const siteBase = '/'
